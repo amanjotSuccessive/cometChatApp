6eef297b1ff9e95cebd8eed5b4034ec0a1995e56
amanjotsinghdhunna <amanjot.singh@successive.tech>
2020-01-16 19:01:55 +0530

    improvement test

---
+++ index.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import App from "./src/App";
+import './ccstyles.css';
+
+export default App;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import App from "./App";
-import './ccstyles.css';
-
-export default App;
