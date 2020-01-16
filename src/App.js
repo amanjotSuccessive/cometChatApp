c45b1f6e9db5297ea11e45f9d8e678185f6ce58f
amanjotsinghdhunna <amanjot.singh@successive.tech>
2020-01-16 17:08:36 +0530

    initial commit, export cometchatapp

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { AppProvider } from "@shopify/polaris";
 import './App.css';
 import Base from "./components/cometchat/Base";
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
+const App = props => {
   return (<AppProvider>
-    <Router>
-      <div>
-        <Route path="/:uid" component={Base} />
-      </div>
-    </Router>
+    <div>
+      <Base props={props} />
+    </div>
   </AppProvider>);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 import './ccstyles.css';
 
-  ReactDOM.render(
-    <App/>,   
-    document.getElementById('root')
-  );
+export default App;
