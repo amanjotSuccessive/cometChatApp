59d371b346af7c94ddcb7649ef3e7708ad928c9b
amanjotsinghdhunna <amanjot.singh@successive.tech>
2020-01-16 13:35:58 +0530

    WIP, initial commit

---
+++ src/constants.js
@@ -0,0 +1,30 @@
+//******** NOTE : THIS IS THE LATEST VERSION OF COMETCHAT - {currently - V2}, app keys created using v1.* are not supported. **********/
+/* 
+    APP CONSTANTS
+*/
+
+export const CC_APP_ID = "12388487f11d670"; //Get your APP ID from dashboard and replace
+export const CC_API_KEY = "821c5191dadd134bc96c7bf4b9be9f85b5133bdf"; //Get your APP ID from dashboard and replace
+export const CC_API_REGION = "us";  //Get your APP REGION from dashboard and replace
+
+/* 
+    LISTENER CONSTANTS
+*/
+export const LISTENER_NEW_MESSAGE = "LISTENER_NEW_MESSAGE";
+export const LISTENER_REAL_TIME_RECIEPTS = "LISTENER_REAL_TIME_RECIEPTS";
+export const LISTENER_NEW_MESSAGE_GROUP = "LISTENER_NEW_MESSAGE_GROUP";
+export const LISTENER_NEW_CALL = "LISTENER_NEW_CALL";
+export const LISTENER_TYPING_INDICATOR = "LISTENER_TYPING_INDICATOR";
+export const LISTENER_RT_PRESENCE = "LISTENER_RT_PRESENCE";
+export const LISTENER_RT_MSG_EDIT = "LISTENER_RT_MSG_EDIT";
+export const LISTENER_RT_MSG_DELETE = "LISTENER_RT_MSG_DELETE";
+export const RT_GROUP_MEMBER_ACTIONS = "RT_GROUP_MEMBER_ACTIONS";
+
+/* 
+    MESSAGE TYPE CONSTANTS
+*/
+export const MESSAGE_TYPE_MEDIA = "1";
+export const MESSAGE_TYPE_IMAGE = "2";
+export const MESSAGE_TYPE_VIDEO = "3";
+export const MESSAGE_TYPE_AUDIO = "4";
+export const MESSAGE_TYPE_FILE  = "5";
